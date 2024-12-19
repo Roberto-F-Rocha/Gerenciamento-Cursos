4d03bbe7df7922207ba60d0990f86efe686fad8d
Roberto_F.rocha <115853601+Roberto-F-Rocha@users.noreply.github.com>
2024-12-19 16:49:26 -0300

    Delete users directory

--- users/api/permissions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import permissions
-
-class IsProfessor(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.groups.filter(name="Professores").exists()
